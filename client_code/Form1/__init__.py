2ef4e4e5b2809499d8dcf4db7f5484b20a422fa5
aliamin745@gmail.com <aliamin745@gmail.com>
2023-10-27 15:49:38 +0000

    Edited settings, theme/parameters.yaml and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -15,3 +15,8 @@ class Form1(Form1Template):
 
 
     # Any code you write here will run before the form opens.
+
+  def label_1_show(self, **event_args):
+    """This method is called when the Label is shown on the screen"""
+    pass
+
