fd8fb6a537a9e5a38023beec02343d22ec7dfc1d
aliamin745@gmail.com <aliamin745@gmail.com>
2023-10-29 16:05:27 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,8 +1,7 @@
 from ._anvil_designer import Form1Template
 
 from anvil import *
-
-
+import anvil.server
 
 class Form1(Form1Template):
 
@@ -12,11 +11,22 @@ class Form1(Form1Template):
 
     self.init_components(**properties)
 
-
-
     # Any code you write here will run before the form opens.
 
   def label_1_show(self, **event_args):
     """This method is called when the Label is shown on the screen"""
     pass
+
+  def process_button_click(self, **event_args):
+    res = anvil.server.call('output', "SOS")
+    if res:
+      self.data_view.text = res
+    else:
+      self.data_view = "Error in fetching"
+    
+  def file_loader_1_change(self, file, **event_args):
+      result = anvil.server.call('my_image_classifier', file)
+    
+
+    
 
