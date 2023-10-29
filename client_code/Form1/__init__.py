ed1cb9f648c10e072574db1a84783e8f80037297
aliamin745@gmail.com <aliamin745@gmail.com>
2023-10-29 20:22:23 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -6,27 +6,22 @@ import anvil.server
 class Form1(Form1Template):
 
   def __init__(self, **properties):
-
-    # Set Form properties and Data Bindings.
-
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
-
-  def label_1_show(self, **event_args):
-    """This method is called when the Label is shown on the screen"""
-    pass
 
   def process_button_click(self, **event_args):
-    res = anvil.server.call('output', "SOS")
+    res = anvil.server.call('show_travel_data')
     if res:
       self.data_view.text = res
     else:
       self.data_view = "Error in fetching"
     
   def file_loader_1_change(self, file, **event_args):
-    result = anvil.server.call('process_file', file)
-    # print(result)
+    anvil.server.call('process_file', file)
+
+  def to_input_copy_pressed_enter(self, **event_args):
+    pass
+
     
 
     
