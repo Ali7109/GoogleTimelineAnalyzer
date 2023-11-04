1d85f66ce08be6e7d0b5e85e2da9807662b113a0
aliamin745@gmail.com <aliamin745@gmail.com>
2023-11-04 15:51:59 +0000

    Edited theme/parameters.yaml and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,18 +9,57 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
 
-  def process_button_click(self, **event_args):
-    res = anvil.server.call('show_travel_data')
-    if res:
-      self.data_view.text = res
-    else:
-      self.data_view = "Error in fetching"
+  def number_of_travels_click(self, **event_args):
+    from_text = self.from_input.text
+    to_text = self.to_input.text
     
-  def file_loader_1_change(self, file, **event_args):
-    anvil.server.call('process_file', file)
+    self.from_input.text = ""
+    self.to_input.text = ""
+    
+    if not from_text or not to_text:
+      return alert("Please enter valid from and to locations")
+    
+    res = anvil.server.call('calculate_travels', from_text, to_text)
+    
+    if res or res == 0:
+      self.output_title.visible = True
+      self.output_label_showcase.visible = True
+      self.output_label_showcase.text = "#Travels from " + from_text + " to " + to_text + " = " + str(res)
+    else:
+      alert("Data not ready, please reupload file")
 
-  def to_input_copy_pressed_enter(self, **event_args):
-    pass
+  def entries_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    to_location = self.entries_input.text
+    self.entries_input.text = ""
+
+    if not to_location:
+      return alert("Please entervalid entry location")
+
+    res = anvil.server.call('calculate_entries', to_location)
+    if res or res == 0:
+      self.output_title.visible = True
+      self.output_label_showcase.visible = True
+      self.output_label_showcase.text = "Number of Entries to " + to_location + " = " + str(res)
+    else:
+      alert("Data not ready, please reupload file")
+   
+
+  
+  def file_loader_1_change(self, file, **event_args):
+
+    if not file:
+      self.output_card.visible = False
+    
+    process_result = anvil.server.call('process_file', file)
+    if process_result:
+      self.output_card.visible = True
+    else:
+      alert("Error occured please try again")
+
+
+
+
 
     
 
